1170e23e78a7edfdc7983cc921649a6ca3ae68d1
KarimCarl <cndungu109@gmail.com>
2023-05-10 10:06:24 +0300

    New revised solution

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
+#Samples
+# def pet(name)
+#     binding.pry
+#     puts "Hello #{name}!"
+# end
+
+# pet("Jamjam")
+# pet("Fatouu")
+# pet("Kifah")
+
+
+#This method takes a param and adds 2 to it
 def plus_two(num)
-	num + 2
+    num + 2
+    #stops execution at this point to check the code
+    binding.pry
 end
+
+plus_two(3)
+#3 the entered param + 2 = 5
